c1d100bee8d5ee30b0960520d6fa09fc1fd5f210
matbott <matiasfusaro@gmail.com>
2025-06-25 15:24:54 -0300

    Update const.py

--- custom_components/inumet_uruguay/const.py
+++ custom_components/inumet_uruguay/const.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 DOMAIN = "inumet_uruguay"
 NAME = "Inumet Uruguay"
-VERSION = "3.0.0"
+VERSION = "3.0.2"
 
 # URLs de la API
 ESTADO_ACTUAL_URL = "https://www.inumet.gub.uy/reportes/estadoActual/datos_inumet_ui_publica.mch"
