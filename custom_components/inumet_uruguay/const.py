934dfdba3460c7382d1d2f4d9efb1b27b59157ed
matbott <matiasfusaro@gmail.com>
2025-06-25 15:43:53 -0300

    Update const.py

--- custom_components/inumet_uruguay/const.py
+++ custom_components/inumet_uruguay/const.py
@@ -3,13 +3,13 @@ from datetime import timedelta
 
 DOMAIN = "inumet_uruguay"
 NAME = "Inumet Uruguay"
-VERSION = "3.0.5"
+VERSION = "3.0.6"
 
 # URLs de la API
 ESTADO_ACTUAL_URL = "https://www.inumet.gub.uy/reportes/estadoActual/datos_inumet_ui_publica.mch"
 ALERTS_URL = "https://w2b.inumet.gub.uy/oapi/collections/urn:wmo:md:uy-inumet:cap-alerts/items?f=json"
 FORECAST_URL = "https://www.inumet.gub.uy/reportes/pronosticos/pronosticoV4.json"
+GENERAL_ALERTS_URL = "https://inumet.gub.uy/reportes/riesgo/advGral.mch" # <-- URL NUEVA
 
-# --- LÍNEA MODIFICADA ---
-# Intervalo de actualización por defecto en minutos
+# Intervalo de actualización
 DEFAULT_UPDATE_INTERVAL = 15
